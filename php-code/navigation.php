d55381c932d4d36a08658bcf2ee1252b7c9f7c13
Biagio Arobba <barobba@gmail.com>
2012-06-20 22:46:46 -0600

    Mass update.
    Signed-off-by: Biagio Arobba <barobba@gmail.com>

--- php-code/navigation.php
+++ php-code/navigation.php
@@ -3,7 +3,7 @@
   <li><a href="about.php">About NEE</a></li>
   <li><a href="blog.php">Blog</a></li>
   <li><a href="stars.php">Shining, Rising, and Setting Stars</a></li>
-  <li><a href="profile-board.php">Our People &amp; Supporters</a></li>
+  <li><a href="profiles.php">Our People &amp; Supporters</a></li>
   <li><a href="programs.php">Projects &amp; Programs</a></li>
   <li><a href="profile-lecturers.php">Speakers &amp; Consultants Bureau</a></li>
   <li><a href="contact-us.php">Contact Us</a></li>
--- profiles.php
+++ profiles.php
@@ -1,18 +1,12 @@
 <?php $page['title'] = 'Profiles' ?>
 <?php require_once 'php-code/template-top.php' ?>
 
-  <style>
-    .content {
-      height: 363px;
-    }
-  </style>
-
-	<h2 id="page-title">Who We Are</h2>
+	<h2 id="page-title">Our People &amp; Supporters</h2>
 	
 	<ul>
 	  <li><a href="profile-glover.php">Dr. John Henry Glover</a></li>
 	  <li><a href="profile-board.php">Board of Directors</a></li>
-	  <li><a href="profile-lecturers.php">Lecturers &amp; Consultants</a></li>
+	  <li><a href="profile-lecturers.php">Speakers &amp; Consultants Bureau</a></li>
 	</ul>
 	
 <?php require_once 'php-code/template-bottom.php' ?>
